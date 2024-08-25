27c3a9a0eee00fcd569a06c65fbda24b3b8d1764
Laurent Abbal <laurent@abbal.com>
2024-08-25 11:07:15 +0900

    ~

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 	@include('inc-meta')
+	<link href="https://fonts.googleapis.com/css2?family=Latin+Modern+Roman:wght@400;700&display=swap" rel="stylesheet">
     <title>{{ config('app.name', 'Laravel') }}</title>
 	<style>
 		body {
@@ -50,11 +51,24 @@
 
 		</div><!-- row -->
 
+		<div class="row mt-4">
+			<div class="col-md-10 offset-md-1" style="font-family: 'Latin Modern Roman', serif;font-size:20px;text-align:justify;">
+				<p>Depuis 2017, le <a href="https://www.lfitokyo.org/" target="_blank">Lycée Français International de Tokyo</a> participe à la <a href="https://www.sciencescope.org/events-2/jfr/jfr2023/" target="_blank">Journée Francophone de la Recherche (JFR)</a> organisée depuis bientôt trois décennies par <a href="https://www.sciencescope.org/" target="_blank">Sciencescope</a>, l'association des étudiants et chercheurs francophones au Japon, avec le soutien de l'<a href="http://www.ambafrance-jp.org/" target="_blank">Ambassade de France</a>, du <a href="https://tokyo.cnrs.fr/" target="_blank">CNRS</a> et de l'<a href="http://www.mfj.gr.jp/" target="_blank">Institut Français de Recherches sur le Japon - Maison Franco-Japonaise (IFRJ-MFJ)</a>. L'expérience de ces dernières années ayant été concluante, il a été décidé d'organiser, avec le soutien de l'<a href="https://www.aefe.fr/" target="_blank">AEFE</a>, une Journée Francophone de la Recherche des Lycéen(ne)s, ouverte à tous les lycées français de la zone Asie-Pacifique, le 13 décembre 2024. Cette première édition aura lieu à Tokyo.
+				</p>
+				L'idée est d'offrir à des lycéens la possibilité :
+				<ul>
+					<li>d'assister à des communications</li>
+					<li>de présenter à l'oral et/ou sous forme de poster un travail de recherche solide qu'ils auront mené avec l'aide de leurs enseignants et éventuellement de chercheurs confirmés.</li>
+				</ul>
+				Le financement du projet permettra à des équipes de venir à Tokyo pour cette journée (selon des conditions qui sont encore à définir).
+			</div>
+		</div><!-- row -->
+
 		<div class="row mt-5">
 			<div class="col-md-12 text-center">
-				<img src="{{ asset('img/logo-sciencescope.png') }}" height="60" class="pl-2 pr-2" />
-				<img src="{{ asset('img/logo-lfit.png') }}" height="60" class="pl-2 pr-2" />
-				<img src="{{ asset('img/logo-aefe-zap.png') }}" height="80" class="pl-2 pr-2" />
+				<a href="https://www.sciencescope.org/" target="_blank"><img src="{{ asset('img/logo-sciencescope.png') }}" height="60" class="pl-2 pr-2" /></a>
+				<a href="https://www.lfitokyo.org/" target="_blank"><img src="{{ asset('img/logo-lfit.png') }}" height="60" class="pl-2 pr-2" /></a>
+				<a href="https://www.aefe.fr/" target="_blank"><img src="{{ asset('img/logo-aefe-zap.png') }}" height="80" class="pl-3 pr-2" /></a>
 			</div>
 		</div><!-- row -->
 
