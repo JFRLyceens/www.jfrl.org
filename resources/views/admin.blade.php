438dfbdd96f432a7ab5d57cfbe3abdc1d0ebe629
laurent.abbal <laurent.abbal@aefe.fr>
2024-12-06 09:29:31 +0900

    ~

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -255,11 +255,11 @@ if (Auth::user()->is_admin != 1) {
                                         $documents = File::files($directory);
                                     }
                                 @endphp
-
+                                {{str_pad($etablissement->id, 3, '0', STR_PAD_LEFT)}}
                                 @if (!empty($documents))
                                 <!-- DOCUMENTS -->
                                 <div class="mt-3 font-weight-bold">Documents</div>
-                                {{str_pad($etablissement->id, 3, '0', STR_PAD_LEFT)}}
+                                
                                 <div class="list-group text-monospace mt-2 mb-2">
                                     @foreach($documents as $document)
                                         <li class="list-group-item p-1">
